88a10c76a90aa648b239e2c932afea6575c2a06e
Rafael Nowrotek <mail@benignware.com>
2015-09-04 17:30:02 +0200

    Added test remove useless styles

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,8 @@
 test("Equal", function(assert) {
   
   assert.domEqual(
-    $('<div class="content" id="content" style="background-color: rgb(239, 239, 239); padding: 5px;">Content</div>'),
-    $('<div id="content" class="content" style="padding: 5px; background-color: rgb(239, 239, 239);">Content</div>'),
+    $('<div class="content" id="content" style="background-color: rgb(239, 239, 239); padding: 5px;">   \n   <span>Content</span></div>'),
+    $('<div id="content" class="content" style="padding: 5px; background-color: rgb(239, 239, 239);"><span>Content</span></div>'),
     'Dom content should be equal'
   );
 });
